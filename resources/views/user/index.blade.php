ebf9a0d69e8e83998f99f13d0377e8ae309c75c0
陳進益 <>
2021-07-31 17:18:06 +0800

    新增前端 header

---
+++ resources/views/user/index.blade.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta http-equiv="content-type" content="text/html; charset=utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <link rel="stylesheet" type="text/css" href="./asset/css/common.css">
+  <link rel="stylesheet" type="text/css" href="./asset/css/header.css">
+  <link rel="stylesheet" type="text/css" href="./asset/icon/fontawesome/css/all.css">
+  <script type="text/javascript" src="./asset/js/vue.js"></script>
+  <script type="text/javascript" src="./asset/js/jQuery/jquery.js"></script>
+
+  <title>賣得好</title>
+</head>
+<body>
+
+  <div id="header">
+    <div id="content">
+      <div id="menu">
+        <div class="menu-sun">
+          <i class="fas fa-home"></i>
+          <span>賣得好</span>
+        </div>
+        <div class="menu-sun">
+          <i class="fas fa-user-circle"></i>
+          <span>會員中心</span>
+        </div>
+        <div class="menu-sun">
+          <i class="fas fa-shopping-cart"></i>
+          <span>購物車</span>
+        </div>
+        <div class="menu-sun">
+          <i class="fas fa-shopping-bag"></i>
+          <span>去買單</span>
+        </div>
+      </div>
+
+      <div id="search-frame">
+        <form @submit.prevent="search">
+          <input type="text" v-model="keyWord" placeholder="我要找商品...">
+          <button><i class="fas fa-search"></i></button>
+        </form>
+      </div>
+    </div>
+  </div>
+
+
+
+  <script type="text/javascript">
+    let app = new Vue({
+        el: '#header',
+        data: {
+          keyWord: null
+        },
+        methods: {
+          search() {
+            console.log(123)
+          }
+        }
+
+    })
+      
+  </script>
+</body>
+</html>
