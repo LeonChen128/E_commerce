2e7090facc4eb68de785d48a0e5cc0d45f9418b3
陳進益 <>
2021-08-22 00:51:36 +0800

    migration 修改

--- database/migrations/2021_07_06_133305_create_user_table.php
+++ database/migrations/2021_07_06_133305_create_user_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreateUserTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('user', function (Blueprint $table) {
             $table->id();
 
             $table->string('name', 190)->comment('使用者名稱');
@@ -21,7 +21,8 @@ class CreateUsersTable extends Migration
             $table->string('password', 190)->comment('使用者密碼');
             $table->string('token', 190)->comment('使用者 token');
 
-            $table->timestamps();
+            $table->timestamp('updated_at')->useCurrent();
+            $table->timestamp('created_at')->useCurrent();
         });
     }
 
@@ -32,6 +33,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('user');
     }
 }
--- database/migrations/2021_08_21_070245_create_product_table.php
+++ database/migrations/2021_08_21_070245_create_product_table.php
@@ -19,10 +19,12 @@ class CreateProductTable extends Migration
 
             $table->string('title', 190)->nullable(false)->comment('商品標題');
             $table->text('description')->nullable()->comment('商品描述');
+            $table->enum('category', ['3c', 'food', 'daily_use', 'others'])->comment('商品分類');
             $table->integer('price')->unsigned()->nullable(false)->comment('商品價格');
-            $table->string('img', 190)->nullable(false)->comment('商品圖片');
+            $table->string('img', 190)->default('')->comment('商品圖片');
 
-            $table->timestamps();
+            $table->timestamp('updated_at')->useCurrent();
+            $table->timestamp('created_at')->useCurrent();
         });
     }
 
