84d58bb9ba68b3d48d15c23f769570427256a83d
陳進益 <a781282000@gmail.com>
2021-09-01 16:04:38 +0800

    新增 validate

--- app/Http/Controllers/api/ProductController.php
+++ app/Http/Controllers/api/ProductController.php
@@ -15,20 +15,20 @@ class ProductController extends \App\Http\Controllers\Controller
     public function index(Request $request)
     {
         $params = $this->validate($request, [
-            'keyWord' => '',
-            'offset' => ''
+            'keyWord' => 'string|nullable|max:190',
+            'offset' => 'integer|min:0'
         ]);
 
-        $build = Product::orderBy('updated_at', 'desc');
-
-        $build = $params['keyWord'] ? $build->where('title', 'like', '%' . $params['keyWord'] . '%') : $build;
+        $build = isset($params['keyWord']) && $params['keyWord']
+            ? Product::where('title', 'like', '%' . $params['keyWord'] . '%') : new Product();
         
         $allCount = $build->count();
         
-        $build = ($params['offset'] && is_numeric($params['offset'])) ? $build->skip($params['offset']) : $build;
+        $build = isset($params['offset']) && $params['offset']
+            ? $build->skip($params['offset']) : $build;
 
         return response()->json([
-            'products' => ($products = $build->take(10)->get()->map(function($product) {
+            'products' => $products = $build->take(10)->orderByDesc('updated_at')->get()->map(function($product) {
                 return [
                     'id' => $product->id,
                     'user_id' => $product->user_id,
@@ -37,10 +37,10 @@ class ProductController extends \App\Http\Controllers\Controller
                     'category' => $product->category,
                     'price' => $product->price,
                     'img' => config('app.user_root') . $product->user_id . '/' . $product->img,
-                    'date' => $product->updated_at->format('U'),
+                    'date' => $product->updated_at->format('Y/m/d h:i:s'),
                 ];
-            })),
-            'offset' => $offect = ($params['offset'] ? ($params['offset'] + $products->count()) : $products->count()),
+            }),
+            'offset' => $offect = isset($params['offset']) && $params['offset'] ? ($params['offset'] + $products->count()) : $products->count(),
             'more' => ($offect >= $allCount) ? false : true
         ]);
     }
